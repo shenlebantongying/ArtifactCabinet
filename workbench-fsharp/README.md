33ea51631df5bf93104635d16464c27a80d30931
shenleban tongying <shenlebantongying@gmail.com>
2021-09-09 22:44:16 -0400

    import fsharp

---
+++ workbench-fsharp/README.md
@@ -0,0 +1,18 @@
+# How to run fsharp?
+
+repl: `dotnet fsi` -> You can send selection of code to the repl in Jetbrains Rider!
+
+## As a script
+
+`dotnet fsi ****.fsx` -> no need for `[<EntryPoint>]` at all
+
+or
+```
+#!/usr/bin/env dotnet fsi
++things
+```
+
+## As a program
+
++ Single Entry Point (aka main function)
++ use the `dotnet` cli to create libraries or things
---
+++ workbench-fsharp/playground/Program.fs
@@ -0,0 +1,21 @@
+open System
+
+// A few higher order functions
+
+[<EntryPoint>]
+let main argv =
+    // Folding
+
+    let testList = [1..10]
+    let sumlist = List.fold (fun acc elem -> acc + elem) 0 testList
+    printfn "%d" sumlist
+
+    let testData =[("cats",3);
+                   ("dogs",5);
+                   ("mice",3)]
+    
+    let countPairs = List.fold (fun acc (name,num) -> acc+num) 0 testData
+    printfn "number of animals %d" countPairs
+    
+    
+    0 // return an integer exit code
