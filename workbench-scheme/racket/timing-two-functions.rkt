d746324411636d091c0c6b67e586d4e34b89b868
shenleban tongying <shenlebantongying@gmail.com>
2021-11-17 16:28:27 -0500

    racket profiling

---
+++ workbench-scheme/racket/timing-two-functions.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define target-length #e5e4)
+
+(define (A current-list new-element)
+  (append current-list (list new-element)))
+
+(define (B current-list new-element)
+  (reverse (cons new-element  (reverse current-list))))
+
+(define (profile function)
+  (printf "~a: " (object-name function))
+  (time
+   (void (for/fold ([l null])
+                   ([n (in-range target-length)])
+           (function l n)))))
+
+(profile A)
+(profile B)
