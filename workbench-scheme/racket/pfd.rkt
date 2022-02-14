e88ae3fb489d1bada71bac4d205fe0b932241733
shenleban tongying <shenlebantongying@gmail.com>
2022-02-14 15:59:11 -0500

    a5 racket

---
+++ workbench-scheme/racket/pfd.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname pfd) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (aux y acc)
+  (if (zero? (remainder y acc))
+      acc
+      (aux y (add1 acc))))
+
+(define (obtain-mini-fact x)
+  (aux x 2))
+
+(define (pfd num)
+  (cond
+    [(= num (obtain-mini-fact num)) (list num)]
+    [else (cons
+           (obtain-mini-fact num)
+           (pfd (/ num (obtain-mini-fact num))))]))
+
+(pfd 24)
+(pfd 42) ;; (list 2 3 7)
