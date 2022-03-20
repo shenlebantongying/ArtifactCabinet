3d847f337abd039f1e1ea6437b79d24762bbc7f9
shenleban tongying <shenlebantongying@gmail.com>
2022-03-20 03:00:46 -0400

    the why of Y

---
+++ workbench-scheme/the-why-of-Y-length.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+;; A Lecture on the Why of Y by Matthias Felleise
+
+(
+ ((λ (le)
+    ((λ (mk-length)
+       (mk-length mk-length))
+     (λ (mk-length)
+       (le (λ (x)
+             ((mk-length mk-length) x))))))
+  (λ (length)
+    (lambda (l)
+      (if (empty? l) 0
+          (add1 (length (cdr l)))))))
+ (list 1 2 3 4 6 7)
+ )
+
+;; generic Y combinator
+
+(define Y
+  (lambda (fun)
+    ((lambda (f)
+       (fun (lambda (x) ((f f) x))))
+     (lambda (f)
+       (fun (lambda (x) ((f f) x)))))))
+
+((Y (λ (length)
+      (lambda (l)
+        (if (empty? l) 0
+            (add1 (length (cdr l)))))))
+ (list 1 2 3 4))
