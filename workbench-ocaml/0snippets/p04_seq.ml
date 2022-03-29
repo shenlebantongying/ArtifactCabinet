8f6986a2e34aa22fb130b8b13a847026468a8299
shenleban tongying <shenlebantongying@gmail.com>
2022-03-28 22:43:55 -0400

    ocaml : .md to .org

--- workbench-ocaml/0snippets/p04_seq.ml
+++ workbench-ocaml/0snippets/p04_seq.ml
@@ -1,6 +1,7 @@
 open List;;
 
 (* Played on OCaml 4.14 *)
+(* seq is kind of lazy list *)
 
 type 'a seq =
   | Nil
@@ -18,8 +19,10 @@ head (tail (tail it));;
 let rec get n s=
   match n, s with
   | 0, _ -> []
-  | n, Nil -> []
+  | _, Nil -> []
   | n, Cons (x, xf) -> x :: get (n-1) (xf ());;
 
 
 get 10 (from 60);;
+
+(* TODO: read Lecture 9 from Madhavapeddy's OCaml course *)
