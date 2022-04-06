fbdbdf7c4180d43c681b7c781b871c2ee7b7fffb
shenleban tongying <shenlebantongying@gmail.com>
2022-04-06 19:16:23 -0400

    fact

--- workbench-scheme/cps/factorial.rkt
+++ workbench-scheme/cps/factorial.rkt
@@ -15,19 +15,20 @@
 
 (equal? (fact 10 (λ (x) x)) (factorial 10))
 
-;; craft a new continuation, based on the previous one, and pass that to our
-;; recursive call
-
-(fact 10 (λ (x) x))
-
 ;; eval steps:
 (fact 3 (λ (x) x))
-
-;; => (fact 2 (λ (x) ((λ (x) x) (* 3 x)))
-;; -> (fact 2 (λ (x) (* 3 x)))
-
-;; => (fact 1 (λ (x) ((λ (x) (* 3 x)) (* 2 x)))
-;; -> (fact 1 (λ (x) (* 3 (* 2 x))))
+;        |
+;        *-------------*
+;                      |
+;                      v
+;; -> (fact 2 (λ (x) ((λ (x) x) (* 3 x)))
+;; => (fact 2 (λ (x) (* 3 x)))
+;             |
+;             *--------*
+;                      |
+;                      v
+;; -> (fact 1 (λ (x) ((λ (x) (* 3 x)) (* 2 x)))
+;; => (fact 1 (λ (x) (* 3 (* 2 x))))
 
 ;; -> (* 3 (* 2 1))
-;; -> 6
+;; => 6
