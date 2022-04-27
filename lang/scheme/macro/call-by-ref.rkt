fed7ae4f1ec2c3fdd892b4919663ec1d3eed7f81
shenleban tongying <shenlebantongying@gmail.com>
2022-04-27 05:19:12 -0400

    weblog.org init

--- lang/scheme/macro/call-by-ref.rkt
+++ lang/scheme/macro/call-by-ref.rkt
@@ -2,3 +2,4 @@
 
 
 ;; TODO: call-by-ref see -> https://www.cs.utah.edu/plt/slideshow/macro-slides/talk.pdf
+;; Actually -> https://docs.racket-lang.org/guide/pattern-macros.html#%28part._pattern-macro-example%29
