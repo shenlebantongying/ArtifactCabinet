4627718e39c25ee2c1d5b2710ac18be9ad180f3b
shenleban tongying <shenlebantongying@gmail.com>
2022-05-03 13:58:18 -0400

    offset C

--- lang/c/README.md
+++ lang/c/README.md
@@ -19,4 +19,6 @@
 + glob.h https://man7.org/linux/man-pages/man3/glob.3.html
 
 # Links
-Good C overview: The Descent to C by Simon Tatham https://www.chiark.greenend.org.uk/~sgtatham/cdescent/
+Good C overview: The Descent to C by Simon Tatham <https://www.chiark.greenend.org.uk/~sgtatham/cdescent/>
+
+The Lost Art of Structure Packing <http://www.catb.org/esr/structure-packing/>
--- lang/c/general-c/offset_play.c
+++ lang/c/general-c/offset_play.c
@@ -2,30 +2,33 @@
 #include <stddef.h>
 
 struct S {
-    char c[8];
+    char c[1];
     int d;
 };
 
 struct S2 {
-    char c[10];
+    char c[7];
     int d;
 };
 
 struct S3 {
-    char c[17];
+    char c[9];
     int d;
 };
 
+struct S4 {
+    char c[12];
+    int d;
+};
+
+// the memory is aligned to 4bytes chunk, if there are empty, they will be padded.
+// 64bits = 8 bytes
 
 int main(){
-    printf("%zu\n", offsetof(struct S,c));
     printf("%zu\n", offsetof(struct S,d));
-
-    printf("%zu\n", offsetof(struct S2,c));
     printf("%zu\n", offsetof(struct S2,d));
-
-    printf("%zu\n", offsetof(struct S3,c));
     printf("%zu\n", offsetof(struct S3,d));
+    printf("%zu\n", offsetof(struct S4,d));
 
     printf("%zu\n", sizeof(char));
 
