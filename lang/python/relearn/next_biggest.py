5f07db6ecd0dfffe97507cde8e4d48da51fb7348
shenleban tongying <shenlebantongying@gmail.com>
2022-06-29 15:02:19 -0400

    python big O

---
+++ lang/python/relearn/next_biggest.py
@@ -0,0 +1,21 @@
+def next_biggest(n: int):
+    nl = list(reversed(list(str(n))))
+
+    cur = "0"
+    counter = 0
+    for x in nl:
+        counter += 1
+        if x > cur:
+            cur = x
+        else:
+            break
+
+    temp = nl[0]
+    nl[0] = nl[counter-1]
+    nl[counter-1]=temp
+
+    nl[0:counter-1] = reversed(sorted(nl[0:counter-1]))
+
+    return int("".join(list(reversed(nl))))
+
+print(next_biggest(227543))
