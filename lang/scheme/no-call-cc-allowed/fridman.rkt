b828a559d371353d8a27259bb0b7965c35d687c5
shenleban tongying <shenlebantongying@gmail.com>
2022-08-06 09:23:57 -0400

    extra sample of call cc

--- lang/scheme/no-call-cc-allowed/fridman.rkt
+++ lang/scheme/no-call-cc-allowed/fridman.rkt
@@ -21,3 +21,15 @@
  8)
 
 (+ 1 (/ (+8^ 10) 0)) ;; => always 18, note that the outer calculations doesn't matter
+
+;; Call/cc for early return
+;; http://bewatermyfriend.org/p/2019/002/
+
+(call/cc (lambda (return) ;; the call/cc is called with an arg
+            (foldr (lambda (x acc)
+                    (if (zero? x)
+                      (return 0)
+                      (* x acc)))
+                  (*)
+                  '(1 2 3 4 0 6 7 8 9 10)))) ;; this program exist early with the 5th zero
+
