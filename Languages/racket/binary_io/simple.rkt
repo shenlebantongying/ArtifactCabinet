54821232cb57e28f4d21b5be22f88cb6ac73e3da
shenleban tongying <shenlebantongying@gmail.com>
2023-05-19 12:50:00 -0400

    r io

---
+++ Languages/racket/binary_io/simple.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (r in)
+  (let ([b (read-byte)])
+    (if (eq? b eof)
+        (display "<end of file>")
+        (begin (writeln (number->string b 16)) (r in)))))
+
+(with-input-from-file "ok.txt"
+  (lambda ()
+    (r (current-input-port))))
