f45489ddc1531e0473e6ca98a6f0787e50f5a2c3
shenleban tongying <shenlebantongying@gmail.com>
2023-09-24 16:44:48 -0400

    ocaml variant list

---
+++ Languages/ocaml/0snippets/variant_list.ml
@@ -0,0 +1,27 @@
+(* Some interactions between list & variants*)
+
+type myT = One of string | Two of string
+
+let myData = [ One "ok"; Two "what" ]
+
+let unwrap = function
+  | One str -> "Type 1 -> " ^ str
+  | Two str -> "Type 2 -> " ^ str
+
+let rec myRec str_lst =
+  match str_lst with
+  | [] -> None
+  | h :: t ->
+      print_endline (unwrap h);
+      myRec t
+
+let rec rev str_lst =
+  let rec aux acc lst =
+    match lst with [] -> acc | h :: t -> aux (h :: acc) t
+  in
+  aux [] str_lst
+;;
+
+myRec myData
+
+(* List.iter (Printf.printf "%s ") (rev myData);; *)
