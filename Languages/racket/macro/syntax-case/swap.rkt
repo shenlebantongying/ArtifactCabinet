c7b8cbcac06ba4e407032b8a5a421242fdf0c190
shenleban tongying <shenlebantongying@gmail.com>
2023-10-22 03:48:47 -0400

    basic syntax-case

---
+++ Languages/racket/macro/syntax-case/swap.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+;; #'template is equal to (syntax template)
+
+(define-syntax (swap stx)
+  (syntax-case stx ()
+    [(swap x y)
+     (if (and (identifier? #'x)
+              (identifier? #'y))
+         (syntax (let ([tmp x])
+             (set! x y)
+             (set! y tmp)))
+         (raise-syntax-error #f "not an identifier" stx #'y #'x))]))
+
+(define x 2)
+(define y 1)
+
+(swap x y)
+
+(values x y)
