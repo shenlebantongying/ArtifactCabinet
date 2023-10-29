9e22798cf3b970afcdca0d06c9d9e68cd75924a2
shenleban tongying <shenlebantongying@gmail.com>
2023-10-29 10:04:45 -0400

    zeromq inproc

---
+++ Playgrounds/zeromq/01_echo_TCP/client.cpp
@@ -0,0 +1,31 @@
+#include <fmt/format.h>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+#include <zmq.h>
+
+int main()
+{
+    void* ctx = zmq_ctx_new();
+    void* requester = zmq_socket(ctx, ZMQ_REQ);
+    int rc = zmq_connect(requester, "tcp://localhost:5555");
+
+    fmt::print("{}\n", rc);
+
+    pid_t pid = getpid();
+
+    for (int i = 0; i < 10; ++i) {
+        std::string msg = fmt::format("(msg {} from {})", i, pid);
+
+        zmq_send(requester, msg.data(), msg.size(), 0);
+
+        fmt::print("Send -> {}\n", msg);
+
+        std::vector<char> rec;
+        rec.resize(256);
+        zmq_recv(requester, rec.data(), rec.size(), 0);
+        std::cout << rec.data() << std::endl;
+        rec.clear();
+    }
+}
