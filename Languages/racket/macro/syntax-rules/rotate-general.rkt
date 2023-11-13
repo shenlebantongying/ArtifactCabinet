266dcc9d38a6cb6776826b9761e13749ab0108db
shenleban tongying <shenlebantongying@gmail.com>
2023-11-13 08:55:37 -0500

    review racket macros

---
+++ Languages/racket/macro/syntax-rules/rotate-general.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+;; Arbitrary number of rotate
+
+;; WHY shift-to NEEDED?
+
+;; TODO: try combine shift-to and rotate and u will know
+
+(define-syntax shift-to
+  (syntax-rules ()
+    ((shift-to (to0 to ...) (from0 from ...) )
+     (let ([tmp from0])
+       (set! to from) ...
+       (set! to0 tmp)))))
+
+(define-syntax rotate
+  (syntax-rules ()
+    ((rotate a b ...)
+     (shift-to (a b ...) (b ... a) ))))
+
+(let ([a 1]
+      [b 2]
+      [c 3]
+      [d 4])
+  (rotate a b c d)
+  (list a b c d)
+  )
+
---
+++ Languages/racket/macro/syntax-rules/rotate3.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+;; Navie rotate3
+(define-syntax swap
+  (syntax-rules ()
+    [(swap x y)
+     (let ([tmp x])
+       (set! x y)
+       (set! y tmp))]))
+
+(define-syntax rotate3
+  (syntax-rules ()
+    [(rotate a b) (swap a b)]
+    [(rotate a b c) (begin
+                      (swap a b)
+                      (swap b c))]))
+
+(let ([x 1]
+      [y 2]
+      [z 3])
+  (rotate3 x y z)
+  (list x y z)
+  )
