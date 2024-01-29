c2153c19ecd94045b730c1b761db1ad5125cf9cb
shenleban tongying <shenlebantongying@gmail.com>
2024-01-29 18:26:38 -0500

    basic QtQuick3D

---
+++ Tech/qt-qml/basic-3d/main.cpp
@@ -0,0 +1,22 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+
+    engine.rootContext()->setContextProperty("what_do_you_mean", "Hello world!");
+
+    engine.loadFromModule("qml3d", "Main");
+
+    return app.exec();
+}
