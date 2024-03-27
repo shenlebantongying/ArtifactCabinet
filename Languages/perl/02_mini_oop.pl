d466b17958071c996d56975ac2686fe2775a2082
shenleban tongying <shenlebantongying@gmail.com>
2024-03-27 16:37:12 -0400

    .

--- Languages/perl/02_mini_oop.pl
+++ Languages/perl/02_mini_oop.pl
@@ -1,8 +1,7 @@
 
 #!/usr/bin/env perl
-#class Point
+use v5.38;
 package Point;
-use strict;
 
 #constructor
 sub new {
@@ -15,17 +14,15 @@ sub new {
     return $self;
 }
 
-#accessor method for Person first name
 sub x {
     my ( $self, $x ) = @_;
     $self->{_x} = $x if defined($x);
     return $self->{_x};
 }
 
-#accessor method for Person last name
 sub y {
     my ( $self, $y ) = @_;
-    $self->{_y} = $lastName if defined($y);
+    $self->{_y} = $y if defined($y);
     return $self->{_y};
 }
 
@@ -35,5 +32,15 @@ sub print {
     #print Person info
     printf( "Name:%s %s\n\n", $self->x, $self->y );
 }
+
+my $pit = Point->new();
+
+
+$pit->x(10);
+$pit->y(20);
+
+
+$pit->print();
+
 
 1;
